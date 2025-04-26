0da1e52f971ffc1248743901b22d618a69e5f186
LujainAlabbad <148874720+LujainAlabbad@users.noreply.github.com>
2025-04-26 15:56:34 +0300

    whitespace

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -3,25 +3,25 @@
  * Homepage 
 */
 exports.homepage = async (req, res) => {
-    const locals = {
-      title: "NodeJs Notes",
-      description: "Free NodeJS Notes App.",
-    }
-    res.render('index', {
-      locals,
-      layout: '../views/layouts/front-page'
-    });
+  const locals = {
+    title: "NodeJs Notes",
+    description: "Free NodeJS Notes App.",
   }
-  
-  
-  /**
-   * GET /
-   * About 
-  */
-  exports.about = async (req, res) => {
-    const locals = {
-      title: "About - NodeJs Notes",
-      description: "Free NodeJS Notes App.",
-    }
-    res.render('about', locals);
+  res.render('index', {
+    locals,
+    layout: '../views/layouts/front-page'
+  });
+}
+
+
+/**
+ * GET /
+ * About 
+*/
+exports.about = async (req, res) => {
+  const locals = {
+    title: "About - NodeJs Notes",
+    description: "Free NodeJS Notes App.",
   }
+  res.render('about', locals);
+}
